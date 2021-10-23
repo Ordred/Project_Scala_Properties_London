70be56be5d231605501f9c0a88fb3552b412d7f3
Jonathan <ordred43@gmail.com>
2021-10-23 16:03:20 +0200

    foundation3

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -6,7 +6,7 @@ import java.io.File
 object Launcher {
   def main(args: Array[String]): Unit = {
     println("Hello World!")
-    val reader = CSVReader.open(new File("C:/Users/Jonathan/IdeaProjects/Project_Scala_Properties_London/08-PropertiesLondon.csv"));
+    val reader = CSVReader.open(new File("./08-PropertiesLondon.csv"));
     var element = reader.readNext();
     while (element != None) {
       println(element);
