b88122a76f1e4d1c1e5b496ff26aea20fe0d9299
Jonathan <ordred43@gmail.com>
2021-10-24 13:46:33 +0200

    some first filters, using traits etc.

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -12,26 +12,62 @@ object Launcher {
 
     val p = t.toList
 
-    var propertyList = new ListBuffer[Property]
+    var propertyList = new ListBuffer[Property];
     var propertyIterator = 1;
 
     while (propertyIterator < p.length) {
       propertyList += (new Property(p(propertyIterator)(0).toInt,p(propertyIterator)(1),p(propertyIterator)(2).toFloat,p(propertyIterator)(3),p(propertyIterator)(4).toInt,p(propertyIterator)(5).toInt,p(propertyIterator)(6).toInt,p(propertyIterator)(7).toInt,p(propertyIterator)(8),p(propertyIterator)(9),p(propertyIterator)(10)));
       propertyIterator = propertyIterator+1;
-      propertyList.foreach(p => println(p))
     }
-    println()
-    println()
 
-    propertyList.filter(p => p.price > 5000000);
+
+    def filterByType(propertyType:String, list:ListBuffer[Property]) = list.filter(p => p.propertyType.contains(propertyType))
+
+    def filterByPrice(price:Float, list:ListBuffer[Property]) = list.filter(p => p.price < price)
+
+    var filtered = filterByType("House", propertyList);
+    filtered = filterByPrice(475000, filtered);
+
+    println(filtered);
+
+
   }
 }
 
-case class Property(id:Int,propertyName:String,price:Float,propertyType:String,number:Int,bedrooms:Int,bathrooms:Int,receptions:Int,
-                    location:String,city:String,postal:String)
+case class Property(id:Int,propertyName:String,price:Float,propertyType:String,sqFt:Int,bedrooms:Int,bathrooms:Int,receptions:Int,
+                    location:String,city:String,postal:String) extends Address with Luxurious {
+  override def toString: String = id.toString+"\t"+propertyName+"\t"+price.toString+"\t"+pricePerSQFt.toString+"\t"+propertyType+"\t"+sqFt.toString+"\t"+bedrooms.toString+"\t"+bathrooms.toString+"\t"+
+    receptions.toString+"\t"+location+"\t"+city+"\t"+postal+"\t"+isLuxurious.toString+"\n"
+}
 
 trait Address {
   val location:String;
   val city:String;
-  val postal: String;
+  val postal:String
+}
+
+trait Luxurious {
+  val price:Float;
+  val sqFt:Int;
+  val pricePerSQFt = price/sqFt;
+  def isLuxurious = pricePerSQFt > 1000;
+}
+
+
+
+case class PropertyList() extends ListBuffer[PropertyList] with Filters {
+
+  override val list = this;
+
+}
+
+trait Filters {
+
+  val list:PropertyList;
+
+  def filterByType(propertyType:String, list:ListBuffer[Property]) = list.filter(p => p.propertyType.contains(propertyType))
+
+  def filterByPrice(price:Float, list:ListBuffer[Property]) = list.filter(p => p.price < price)
+
+
 }
