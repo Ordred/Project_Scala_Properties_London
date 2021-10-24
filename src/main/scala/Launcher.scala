8bc8a07704e2fb8c6d35923c1e04b63e9d98752b
Jonathan <ordred43@gmail.com>
2021-10-24 12:04:26 +0200

    list a bit better

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -1,6 +1,7 @@
 import com.github.tototoshi.csv.CSVReader
 
 import java.io.File
+import scala.collection.mutable.ListBuffer
 
 
 object Launcher {
@@ -10,24 +11,27 @@ object Launcher {
     val t = reader.toStream;
 
     val p = t.toList
-    var property = new Property(p(0)(0),p(0)(1),p(0)(2),p(0)(3),p(0)(4),p(0)(5),p(0)(6),p(0)(7),p(0)(8),p(0)(9),p(0)(10));
-    var propertyList = ArrayBuffer.empty[Property]
+
+    var propertyList = new ListBuffer[Property]
     var propertyIterator = 1;
 
-    println(property)
-
     while (propertyIterator < p.length) {
-      propertyList.addOne(new Property(p(propertyIterator)(0),p(propertyIterator)(1),p(propertyIterator)(2),p(propertyIterator)(3),p(propertyIterator)(4),p(propertyIterator)(5),p(propertyIterator)(6),p(propertyIterator)(7),p(propertyIterator)(8),p(propertyIterator)(9),p(propertyIterator)(10)))
+      propertyList += (new Property(p(propertyIterator)(0).toInt,p(propertyIterator)(1),p(propertyIterator)(2).toFloat,p(propertyIterator)(3),p(propertyIterator)(4).toInt,p(propertyIterator)(5).toInt,p(propertyIterator)(6).toInt,p(propertyIterator)(7).toInt,p(propertyIterator)(8),p(propertyIterator)(9),p(propertyIterator)(10)));
       propertyIterator = propertyIterator+1;
       propertyList.foreach(p => println(p))
     }
+    println()
+    println()
+
+    propertyList.filter(p => p.price > 5000000);
   }
-
-
-
 }
 
-case class Property(id:String,propertyName:String,price:String,propertyType:String,number:String,bedrooms:String,bathrooms:String,receptions:String,
+case class Property(id:Int,propertyName:String,price:Float,propertyType:String,number:Int,bedrooms:Int,bathrooms:Int,receptions:Int,
                     location:String,city:String,postal:String)
 
-//Option(List:)
+trait Address {
+  val location:String;
+  val city:String;
+  val postal: String;
+}
