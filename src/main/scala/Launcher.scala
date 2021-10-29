4bfef7ed9bb1db0ff2c38dffb0d3f179f920a39e
HutterSven <64769368+HutterSven@users.noreply.github.com>
2021-10-29 16:56:20 +0200

    added groupBy !!!untested!!!

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -28,34 +28,40 @@ object Launcher {
 
     //println(Operations(propertyList).groupByType("House"))
 
-    val filters: Filters = Filters(None ,Some("Apartment"),Some(2400000.0), None )
+    val filters: Filters = Filters(None ,Some("Apartment"),Some(2400000.0), None, None)
+
+    val sortList: List[String] = List("Bedrooms", "Price", "Name")
 
     val propertyList2 = init();
 
-    println(mainFilter(filters, propertyList2).count(p => true),mainSort(List("Price", "Name"), mainFilter(filters, propertyList2)))
+    println(mainFilter(filters, propertyList2).count(p => true), mainSort(sortList, mainFilter(filters, propertyList2), true))
 
   }
 
-  def mainSort(sortByList: List[String], propertyList: List[Property]): List[Property] ={
+  def mainSort(sortByList: List[String], propertyList: List[Property], firstIteration: Boolean): List[Property] ={
     def sortByName(sortNameList: List[Property]): List[Property] = sortNameList.sortBy(p => p.propertyName)
     def sortByType(sortTypeList: List[Property]): List[Property] = sortTypeList.sortBy(p => p.propertyType)
     def sortByPrice(sortPriceList: List[Property]): List[Property] = sortPriceList.sortBy(p => p.price)
     def sortByBedrooms(sortBedroomsList: List[Property]): List[Property] = sortBedroomsList.sortBy(p => p.bedrooms)
 
-    var tempList: List[Property] = propertyList;
+    val tempSortByList: List[String] = if(firstIteration){
+      sortByList.reverse;
+    }else{
+      sortByList;
+    }
 
-    val tempSortByList = sortByList.reverse;
+    val tempList: List[Property] = tempSortByList.head match{
+      case "Name" => sortByName(propertyList)
+      case "Type" => sortByType(propertyList)
+      case "Price" => sortByPrice(propertyList)
+      case "Bedrooms" => sortByBedrooms(propertyList)
+    }
 
-    tempSortByList.map(e => {
-      e match{
-        case "Name" => tempList = sortByName(tempList)
-        case "Type" => tempList = sortByType(tempList)
-        case "Price" => tempList = sortByPrice(tempList)
-        case "Bedrooms" => tempList = sortByBedrooms(tempList)
-      }
-    })
-
-    tempList
+    if (tempSortByList.length > 1){
+      mainSort(tempSortByList.slice(1,tempSortByList.length), tempList, false)
+    }else{
+      tempList
+    }
 
   }
 
@@ -64,6 +70,7 @@ object Launcher {
     def filterByType(propertyType:String, typeInputList: List[Property]): Option[List[Property]] = Some(typeInputList.filter(p => p.propertyType.contains(propertyType)))
     def filterByPrice(price:Double, priceInputList: List[Property]): Option[List[Property]] = Some(priceInputList.filter(p => p.price < price))
     def filerByLuxurious(luxInputList: List[Property]): Option[List[Property]] = Some(luxInputList.filter(p => p.isLuxurious))
+    def filterByCity(city:String, cityInputList: List[Property]): Option[List[Property]] = Some(cityInputList.filter(p => p.city.contains(city)))
 
     val nameFilterList: Option[List[Property]] = {
       if(filters.NameFilter.isDefined){
@@ -97,7 +104,34 @@ object Launcher {
       }
     }
 
-    luxFilterList.get
+    val cityFilterList: Option[List[Property]] = {
+      if(filters.CityFilter.isDefined){
+        filterByCity(filters.CityFilter.get, luxFilterList.get)
+      }else{
+        Some(luxFilterList.get)
+      }
+    }
+
+    cityFilterList.get
+  }
+
+  def groupBy(propertyList: List[Property], CriteriaType: String, Criteria: String): Group ={
+    def avgPrice(list: List[Property]): Double ={
+      list.map(_.price).sum / list.length
+    }
+
+    def avgPricePerSQFt(list: List[Property]): Double ={
+      list.map(_.pricePerSQFt).sum / list.length
+    }
+
+    val tempList: List[Property] = CriteriaType match{
+      case "Type" => mainFilter(Filters(None, Some(Criteria), None, None, None), propertyList )
+      case "City" => mainFilter(Filters(None, None, None, None,  Some(Criteria)), propertyList )
+    }
+
+
+
+    Group("Group by: " + Criteria, tempList.count(p => true), tempList.map(_.price).sum, avgPrice(tempList), avgPricePerSQFt(tempList))
   }
 }
 
@@ -108,7 +142,8 @@ case class Property(id:Int,propertyName:String,price:Double,propertyType:String,
 
 }
 
-case class Group(propertyType:String,count:Int,total:Double,averagePrice:Double,averagePriceSQFt:Double);
+case class Group(propertyType:String,count:Int,total:Double,averagePrice:Double,averagePriceSQFt:Double)
+
 
 trait Address {
   val location:String;
@@ -123,7 +158,7 @@ trait Luxurious {
   def isLuxurious = pricePerSQFt > 1000;
 }
 
-case class Filters(NameFilter: Option[String],TypeFilter: Option[String],PriceFilter: Option[Double],LuxuriousFilter: Option[Boolean])
+case class Filters(NameFilter: Option[String],TypeFilter: Option[String],PriceFilter: Option[Double],LuxuriousFilter: Option[Boolean], CityFilter: Option[String])
 
 
 
